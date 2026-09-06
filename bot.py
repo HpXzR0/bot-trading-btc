2f97a96b811b10e34d969189275ec8b0646826da
HpXzR0 <KafaCunha@gmail.com>
2026-09-05 22:01:44 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,26 @@
+import os
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+# Servidor dummy para manter o Render Web Service ativo gratuitamente
+class SimpleHandler(BaseHTTPRequestHandler):
+
+  def do_GET(self):
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write(b"Bot is running 24/7!")
+
+
+def run_dummy_server():
+  port = int(os.environ.get("PORT", 10000))
+  server = HTTPServer(("0.0.0.0", port), SimpleHandler)
+  server.serve_forever()
+
+
+# Inicia o servidor HTTP em uma thread paralela
+threading.Thread(target=run_dummy_server, daemon=True).start()
+
 import ccxt
 import pandas as pd
 import time
